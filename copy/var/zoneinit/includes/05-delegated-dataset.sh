bd2141afd44471a5a6375f88053fd3e22c158ffb
Boris Poblotzki <bp@core.io>
2014-06-26 13:47:10 +0200

    Added basics like mdata-setup, spipe-conf etc by copying...

---
+++ copy/var/zoneinit/includes/05-delegated-dataset.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+UUID=$(mdata-get sdc:uuid)
+DDS=zones/$UUID/data
+
+if ! zfs list $DDS > /dev/null; then
+        # No delegated dataset configured
+        exit 0
+fi
+
+zfs set mountpoint=/var/db/redis/ $DDS
+chown redis /var/db/redis/
