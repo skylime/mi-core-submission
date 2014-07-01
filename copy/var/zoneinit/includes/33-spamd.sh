9a4e96468e2f306555bae182d7ecee1a3dc5cc8a
Thomas Merkel <tm@core.io>
2014-07-01 23:11:25 +0200

    Add exim manifest because it's missing in the pkgs

--- copy/var/zoneinit/includes/33-spamd.sh
+++ copy/var/zoneinit/includes/33-spamd.sh
@@ -2,8 +2,5 @@
 CRON='0 10 * * * /opt/local/bin/sa-update && kill -SIGHUP $(cat /var/spamassassin/spamd.pid)'
 (crontab -l spamd 2>/dev/null || true; echo "$CRON" ) | sort | uniq | sudo -u spamd crontab
 
-# run sa-update once
-sudo -u spamd /opt/local/bin/sa-update
-
 # enable spamd service
 /usr/sbin/svcadm enable svc:/network/spamd
