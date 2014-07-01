d23a975d9afafb10d6ac00b86fb9edf1b75e1694
Thomas Merkel <tm@core.io>
2014-07-01 22:38:26 +0200

    modify cron execute

--- copy/var/zoneinit/includes/33-spamd.sh
+++ copy/var/zoneinit/includes/33-spamd.sh
@@ -1,5 +1,5 @@
 # create cronjob for sa-update
-CRON="0 10 * * * /opt/local/bin/sa-update && kill -SIGHUP $(cat /var/spamassassin/spamd.pid)"
+CRON='0 10 * * * /opt/local/bin/sa-update && kill -SIGHUP $(cat /var/spamassassin/spamd.pid)'
 (crontab -l spamd 2>/dev/null || true; echo "$CRON" ) | sort | uniq | sudo -u spamd crontab
 
 # run sa-update once
