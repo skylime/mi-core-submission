0d14efe949362c31e89beadceb948610457fe8a0
Thomas Merkel <tm@core.io>
2014-07-03 22:58:50 +0200

    add cronjob for sa-update into root user
    * sa-update will run via sudo to be spamd
    * reload of spamd requires root priv. so add full cron as root

--- copy/var/zoneinit/includes/33-spamd.sh
+++ copy/var/zoneinit/includes/33-spamd.sh
@@ -1,6 +1,6 @@
 # create cronjob for sa-update
-CRON='0 10 * * * /opt/local/bin/sa-update && kill -SIGHUP $(cat /var/spamassassin/spamd.pid)'
-(crontab -l spamd 2>/dev/null || true; echo "$CRON" ) | sort | uniq | sudo -u spamd crontab
+CRON='0 10 * * * sudo -u spamd /opt/local/bin/sa-update && kill -SIGHUP $(cat /var/spamassassin/spamd.pid)'
+(crontab -l 2>/dev/null || true; echo "$CRON" ) | sort | uniq | crontab
 
 # enable spamd service
 /usr/sbin/svcadm enable svc:/network/spamd
