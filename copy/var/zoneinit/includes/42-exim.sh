c9522e28ff6f65206e7296c31ef6be5d3b205452
Thomas Merkel <tm@core.io>
2014-10-02 16:29:12 +0200

    new: create cronjob for exim verify script

--- copy/var/zoneinit/includes/42-exim.sh
+++ copy/var/zoneinit/includes/42-exim.sh
@@ -1,1 +1,6 @@
+# create cronjob for exim panic log check and tinydb
+CRON='25 6 * * * /opt/core/bin/exim-cron.sh'
+(crontab -l 2>/dev/null || true; echo "$CRON" ) | sort | uniq | crontab
+
+# enable exim service
 /usr/sbin/svcadm enable svc:/pkgsrc/exim:default
