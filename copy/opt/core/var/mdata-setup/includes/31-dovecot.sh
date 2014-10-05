a05edc62007df25c9c3d2baa6d7f7c347fa7162a
Thomas Merkel <tm@core.io>
2014-10-05 23:12:50 +0200

    chg: rename to proxy_mbox_ variable

--- copy/opt/core/var/mdata-setup/includes/31-dovecot.sh
+++ copy/opt/core/var/mdata-setup/includes/31-dovecot.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Configure dovecot ssl certificates
 
-mdata-get mbox_ssl > /opt/local/etc/dovecot/ssl/dovecot.pem
+mdata-get proxy_mbox_ssl > /opt/local/etc/dovecot/ssl/dovecot.pem
 chmod 400 /opt/local/etc/dovecot/ssl/dovecot.pem
 
-if mdata-get proxy_host 1>/dev/null 2>&1; then
+if mdata-get proxy_mbox_host 1>/dev/null 2>&1; then
 	cat > /opt/local/etc/dovecot/conf.d/core-proxy.conf <<EOF
-proxy=y host=$(mdata-get proxy_host) ssl=any-cert port=993 nopassword=y
+proxy=y host=$(mdata-get proxy_mbox_host) ssl=any-cert port=993 nopassword=y
 EOF
 
 fi
