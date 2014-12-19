1a22543df70bc2b8ad48de922b82e5d35a57ab42
Thomas Merkel <tm@core.io>
2014-12-19 15:32:15 +0100

    fix: issue with .ext file reference from dovecot.conf

--- copy/opt/core/var/mdata-setup/includes/31-dovecot.sh
+++ copy/opt/core/var/mdata-setup/includes/31-dovecot.sh
@@ -5,7 +5,7 @@ mdata-get proxy_mbox_ssl > /opt/local/etc/dovecot/ssl/dovecot.pem
 chmod 400 /opt/local/etc/dovecot/ssl/dovecot.pem
 
 if mdata-get proxy_mbox_host 1>/dev/null 2>&1; then
-	cat > /opt/local/etc/dovecot/conf.d/core-proxy.conf <<EOF
+	cat > /opt/local/etc/dovecot/conf.d/core-proxy.conf.ext <<EOF
 proxy=y host=$(mdata-get proxy_mbox_host) ssl=any-cert nopassword=y
 EOF
 
