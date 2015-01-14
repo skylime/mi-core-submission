982968cdccc66efaefdd3c5f88e6823b292cfb58
Thomas Merkel <tm@core.io>
2015-01-14 16:05:40 +0100

    only start dovecot if mdata variables are used

--- copy/var/zoneinit/includes/30-dovecot.sh
+++ copy/var/zoneinit/includes/30-dovecot.sh
@@ -1,1 +1,6 @@
-/usr/sbin/svcadm enable svc:/pkgsrc/dovecot:default
+# Only enable if mdata variables are set correctly
+if mdata-get proxy_mbox_ssl  1>/dev/null 2>&1 && \
+   mdata-get proxy_mbox_host 1>/dev/null 2>&1; then
+
+	/usr/sbin/svcadm enable svc:/pkgsrc/dovecot:default
+fi
