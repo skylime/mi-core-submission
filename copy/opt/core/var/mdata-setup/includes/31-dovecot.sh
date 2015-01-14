2947d2901ef5466e17ddcf7246c8aa7944ab80f7
Thomas Merkel <tm@core.io>
2015-01-14 16:04:05 +0100

    only if mdata exists

--- copy/opt/core/var/mdata-setup/includes/31-dovecot.sh
+++ copy/opt/core/var/mdata-setup/includes/31-dovecot.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-# Configure dovecot ssl certificates
+# Configure dovecot ssl certificates
 
-mdata-get proxy_mbox_ssl > /opt/local/etc/dovecot/ssl/dovecot.pem
-chmod 400 /opt/local/etc/dovecot/ssl/dovecot.pem
+if mdata-get proxy_mbox_ssl  1>/dev/null 2>&1 && \
+   mdata-get proxy_mbox_host 1>/dev/null 2>&1; then
 
-if mdata-get proxy_mbox_host 1>/dev/null 2>&1; then
+	# Configure ssl certificate and proxy only if required and mdata exists
+	mdata-get proxy_mbox_ssl > /opt/local/etc/dovecot/ssl/dovecot.pem
+	chmod 400 /opt/local/etc/dovecot/ssl/dovecot.pem
+
 	cat > /opt/local/etc/dovecot/conf.d/core-proxy.conf.ext <<EOF
 proxy=y host=$(mdata-get proxy_mbox_host) ssl=any-cert nopassword=y
 EOF
