4dbe4427360d517054da0cfe763ef1437cd827e6
Thomas Merkel <tm@core.io>
2015-02-06 18:41:01 +0100

    config log path and logadm for dovecot

--- copy/var/zoneinit/includes/30-dovecot.sh
+++ copy/var/zoneinit/includes/30-dovecot.sh
@@ -3,4 +3,9 @@ if mdata-get proxy_mbox_ssl  1>/dev/null 2>&1 && \
    mdata-get proxy_mbox_host 1>/dev/null 2>&1; then
 
 	/usr/sbin/svcadm enable svc:/pkgsrc/dovecot:default
+
+	# configure logadm
+	logadm -w /var/log/dovecot/main -p 1d -C 10 -N -o dovecot -g dovenull -m 640 -c
+	logadm -w /var/log/dovecot/info -p 1d -C 10 -N -o dovecot -g dovenull -m 640 -c
+	logadm -w /var/log/dovecot/debug -p 1d -C 10 -N -o dovecot -g dovenull -m 640 -c
 fi
