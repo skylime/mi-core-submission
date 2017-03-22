88d6d32f1915df01ca503ecce11a6cee63015090
Thomas Merkel <tm@core.io>
2017-03-22 17:00:14 +0100

    Support Let's Encrypt if no SSL certificates are provided

--- copy/var/zoneinit/includes/50-dovecot.sh
+++ copy/var/zoneinit/includes/50-dovecot.sh
@@ -1,6 +1,5 @@
 # Only enable if mdata variables are set correctly
-if mdata-get proxy_mbox_ssl  1>/dev/null 2>&1 && \
-   mdata-get proxy_mbox_host 1>/dev/null 2>&1; then
+if mdata-get proxy_mbox_host 1>/dev/null 2>&1; then
 
 	/usr/sbin/svcadm enable svc:/pkgsrc/dovecot:default
 
