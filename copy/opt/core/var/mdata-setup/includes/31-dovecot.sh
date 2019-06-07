29295951167160b22cca7bd645863a0b2d6a1f3c
Thomas Merkel <tm@core.io>
2019-06-07 13:37:59 +0200

    Move spiped and dovecot config from mdata-setup to zoneinit

--- copy/opt/core/var/mdata-setup/includes/31-dovecot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Configure dovecot proxy host
-
-if mdata-get proxy_mbox_host 1>/dev/null 2>&1; then
-	cat > /opt/local/etc/dovecot/conf.d/core-proxy.conf.ext <<EOF
-proxy=y host=$(mdata-get proxy_mbox_host) ssl=yes nopassword=y
-EOF
-fi
--- copy/var/zoneinit/includes/50-dovecot.sh
+++ copy/var/zoneinit/includes/50-dovecot.sh
@@ -1,5 +1,8 @@
 # Only enable if mdata variables are set correctly
 if mdata-get proxy_mbox_host 1>/dev/null 2>&1; then
+	cat > /opt/local/etc/dovecot/conf.d/core-proxy.conf.ext <<-EOF
+		proxy=y host=$(mdata-get proxy_mbox_host) ssl=yes nopassword=y
+	EOF
 
 	/usr/sbin/svcadm enable svc:/pkgsrc/dovecot:default
 
