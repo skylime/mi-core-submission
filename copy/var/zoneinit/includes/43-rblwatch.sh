afa977fdc740f2221d590826aee4c64fbcd91316
Thomas Merkel <tm@core.io>
2019-06-10 22:02:41 +0200

    Replace python rblwatch/rblcheck script with new shell implementation

--- copy/var/zoneinit/includes/43-rblwatch.sh
+++ copy/var/zoneinit/includes/43-rblwatch.sh
@@ -1,3 +1,3 @@
 # create cronjob for rblwatch/rblcheck
-CRON='0 0,6,12,18 * * * /opt/local/bin/rblcheck'
+CRON='0 0,6,12,18 * * * /opt/core/bin/rblcheck.sh'
 (crontab -l 2>/dev/null || true; echo "$CRON" ) | sort | uniq | crontab
