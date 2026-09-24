8517c108a3e171c2d311a7daa5a2579219a3bba5
Thomas Merkel <tm@core.io>
2026-09-24 19:53:08 +0200

    fix: dovecot munin plugin link

--- copy/var/zoneinit/includes/50-dovecot.sh
+++ copy/var/zoneinit/includes/50-dovecot.sh
@@ -16,6 +16,6 @@ if mdata-get proxy_mbox_host 1>/dev/null 2>&1; then
 	/usr/sbin/svcadm enable svc:/pkgsrc/dovecot:default
 
 	# Enable munin-plugin for dovecot
-	ln -sf /opt/local/lib/munin/plugins/dovecot \
+	ln -sf /opt/local/lib/munin/plugins/smartos/dovecot \
 		/opt/local/etc/munin/plugins/dovecot
 fi
